5e282c7f92361916bd60f5de31c4d1241e24af6b
Mohammad Shaikh <94097592+shaikhmd007@users.noreply.github.com>
2023-01-03 10:26:30 +0530

    Add files via upload

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import Search from "../components/Search";
+
+const Banner = () => {
+  return (
+    <section>
+      <div className="flex flex-col lg:flex-row"></div>
+      <Search />
+    </section>
+  );
+};
+
+export default Banner;
